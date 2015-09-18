50b3e50dc79fe3db8f31eaf5bf7607511903db99
Caleb Champlin <caleb@solaire.io>
2015-09-18 16:07:04 +0000

    Executable updates

--- bin/launcher.sh
+++ bin/launcher.sh
@@ -1,12 +1,8 @@
 #!bin/sh
 
-echo $LISTEN_FDS
-echo $LISTEN_PID
-echo $$
-echo $BASHPID
+CONTAINER_ADDR=`etcdctl get solaire.io/containers/php/$1`
 
-CONTAINER_ADDR=`etcdctl get solaire.io/containers/php/test1`
-
-echo $CONTAINER_ADDR
-arr=(/usr/lib/systemd/systemd-socket-proxyd $CONTAINER_ADDR:9000)
+echo Launching Connections to $CONTAINER_ADDR
+#arr=(/usr/lib/systemd/systemd-socket-proxyd $CONTAINER_ADDR:9000)
+arr=(/opt/bin/goproxy $CONTAINER_ADDR:9000)
 exec "${arr[@]}"
--- bin/set-server-info.sh
+++ bin/set-server-info.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
 if CONTAINER_ADDR=`/usr/bin/docker inspect -f '{{.NetworkSettings.IPAddress}}' $1` ; then
-  echo $CONTAINER_ADDR
-  /usr/bin/etcdctl set solaire.io/containers/php/test1 $CONTAINER_ADDR
+  echo Setting Container Address to $CONTAINER_ADDR
+  /usr/bin/etcdctl set solaire.io/containers/php/$1 $CONTAINER_ADDR
+  echo `/usr/bin/etcdctl get solaire.io/containers/php/$1`
+  echo Set Complete
   exit 0
 fi
 exit -1
